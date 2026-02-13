e4401644ed5da9cfb88d7be5b22ac921f28e0294
Serge <sajghaa@gmail.com>
2026-02-13 18:09:07 +0200

    Updated DemoService

---
+++ src/main/java/com/serge/oop/demo/DemoService.java
@@ -0,0 +1,15 @@
+package com.serge.oop.demo;
+
+import com.serge.oop.utils.MathUtils;
+import com.serge.oop.utils.StringUtils;
+import com.serge.oop.utils.ArrayUtils;
+import com.serge.oop.services.LogicService;
+import com.serge.oop.services.RealWorldService;
+
+
+public class DemoService{
+    public void run(){
+        System.out.println("Factorial of 5: " + MathUtils.factorial(5));
+    }
+}
+
--- src/main/java/com/serge/oop/services/LogicService.java
+++ src/main/java/com/serge/oop/services/LogicService.java
@@ -1,4 +1,17 @@
-package main.java.com.serge.oop.services;
+package com.yourname.oop.demo;
+
+import com.yourname.oop.utils.MathUtils;
+import com.yourname.oop.utils.StringUtils;
+import com.yourname.oop.utils.ArrayUtils;
+import com.yourname.oop.services.LogicService;
+import com.yourname.oop.services.RealWorldService;
+
+public class DemoService {
+    public void run(){
+        System.out.println("Factorial 5: " + MathUtils.factorial(5));
+        System.out.println("Is 'level' palindrome? " + StringUtils.isPalindrome("level"));
+    }
+}
 
 public class LogicService {
     
