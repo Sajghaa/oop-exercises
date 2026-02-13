bfa2f3e1e678c96e5b2c2e2540cf5e1d55dd6ad0
Serge <sajghaa@gmail.com>
2026-02-13 17:34:43 +0200

    factorial number program added

--- src/main/java/com/serge/oop/utils/MathUtils.java
+++ src/main/java/com/serge/oop/utils/MathUtils.java
@@ -1,3 +1,18 @@
+package com.serge.oop.utils;
 public class MathUtils {
-    
+    public static long factorial(int number){   
+        // I like to start with simple validation
+        if (number < 0){
+            throw new IllegalArgumentException("Number cannot be negative");
+        }
+
+        long result = 1;
+
+        for (int i = 1; i <= number; i++){
+            result *= i; 
+        }
+
+        return result;
+
+     }
 }
