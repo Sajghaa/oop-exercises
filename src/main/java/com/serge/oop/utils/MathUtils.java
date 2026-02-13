245d63219cdb5a895c1c3b884ed222aefe045dd4
Serge <sajghaa@gmail.com>
2026-02-13 17:57:40 +0200

    factorial app using recursive

--- src/main/java/com/serge/oop/utils/MathUtils.java
+++ src/main/java/com/serge/oop/utils/MathUtils.java
@@ -1,5 +1,6 @@
 package com.serge.oop.utils;
 public class MathUtils {
+    // 1. factorial program with simple loop
     public static long factorial(int number){   
         // I like to start with simple validation
         if (number < 0){
@@ -15,4 +16,17 @@ public class MathUtils {
         return result;
 
      }
+
+    public static long recursive(int number){
+
+        if(number < 0){
+            throw new IllegalArgumentException("Number cannot be negative");
+        }
+
+        if (number <= 1){
+            return 1;
+        }
+
+        return number * recursive(number -1);
+    }
 }
