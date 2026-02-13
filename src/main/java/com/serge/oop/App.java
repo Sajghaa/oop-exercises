23b0eb6aefd79a41ddc7fe4e4449d2c8b85a13d2
Serge <sajghaa@gmail.com>
2026-02-13 18:11:22 +0200

    App.java updated

---
+++ src/main/java/com/serge/oop/App.java
@@ -0,0 +1,10 @@
+package com.serge.oop;
+
+import com.serge.oop.demo.DemoService;
+
+public class App{
+    public static void main(String[] args) {
+        DemoService demo = new DemoService();
+        demo.run();
+    }
+}
